65e3e6e63b5802be68fd925de83a86d8c5e9c25d
Veronika Machova <mach.veronika@gmail.com>
2022-12-29 12:08:12 +0100

    First commit

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,35 @@
+sap.ui.define([
+  "sap/ui/core/mvc/Controller",
+  "sap/m/MessageToast",
+  "sap/ui/model/json/JSONModel",
+  "sap/ui/model/resource/ResourceModel"
+], (Controller, MessageToast, JSONModel, ResourceModel) => {
+  "use strict"
+  return Controller.extend("sap.ui.demo.walkthrough.App",{
+      onInit : function () {
+        const oData = {
+          recipient: {
+            name: "UI5"
+          }
+        };
+        const oModel = new JSONModel(oData);
+        this.getView().setModel(oModel);
+
+        const i18Model = new ResourceModel({
+          bundleName: "sap.ui.demo.walkthrough.i18n.i18n",
+          supportedLocales: [""],
+          fallbackLocale: ""
+        });
+
+        this.getView().setModel(i18Model, "i18n");
+      },
+      onShowHello:  function () {
+
+        const oBundle = this.getView().getModel("i18n").getResourceBundle();
+        const sRecipient = this.getView().getModel().getProperty("/recipient/name");
+        const sMsg = oBundle.getText("helloMsg", [sRecipient]);
+
+        MessageToast.show(sMsg);
+      }
+  })
+})
