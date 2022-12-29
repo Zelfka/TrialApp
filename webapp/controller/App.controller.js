754c1574d833e3bfebb464b0f4af5c861acebdc5
Veronika Machova <mach.veronika@gmail.com>
2022-12-29 14:24:33 +0100

    step 10 finished

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,35 +1,16 @@
 sap.ui.define([
   "sap/ui/core/mvc/Controller",
-  "sap/m/MessageToast",
-  "sap/ui/model/json/JSONModel",
-  "sap/ui/model/resource/ResourceModel"
-], (Controller, MessageToast, JSONModel, ResourceModel) => {
+  "sap/m/MessageToast"
+], function (Controller, MessageToast) {
   "use strict"
-  return Controller.extend("sap.ui.demo.walkthrough.App",{
-      onInit : function () {
-        const oData = {
-          recipient: {
-            name: "UI5"
-          }
-        };
-        const oModel = new JSONModel(oData);
-        this.getView().setModel(oModel);
-
-        const i18Model = new ResourceModel({
-          bundleName: "sap.ui.demo.walkthrough.i18n.i18n",
-          supportedLocales: [""],
-          fallbackLocale: ""
-        });
-
-        this.getView().setModel(i18Model, "i18n");
-      },
-      onShowHello:  function () {
-
-        const oBundle = this.getView().getModel("i18n").getResourceBundle();
-        const sRecipient = this.getView().getModel().getProperty("/recipient/name");
-        const sMsg = oBundle.getText("helloMsg", [sRecipient]);
-
-        MessageToast.show(sMsg);
+  return Controller.extend("sap.ui.demo.walkthrough.App", {
+      onShowHello: function () {
+          // read msg from i18n model
+          var oBundle = this.getView().getModel("i18n").getResourceBundle();
+          var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+          var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+          // Show message
+          MessageToast.show(sMsg);
       }
-  })
-})
+  });
+});
